9e55fbc378a17269f7cd6bcedebdd70b8aaf5eef
Chris Sauve <chrismsauve@gmail.com>
2020-09-16 14:19:39 -0400

    Remove old version of the code

--- packages/mini-react/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const EMPTY_OBJ = {};
-export const EMPTY_ARR = [];
--- packages/mini-react/src/diff/utilities.ts
+++
@@ -1,6 +0,0 @@
-import type {RemoteRoot} from '@remote-ui/core';
-import type {ReactRemoteNode} from '../types';
-
-export function getRemoteRoot(node: ReactRemoteNode): RemoteRoot<any, any> {
-  return 'root' in node ? node.root : node;
-}
