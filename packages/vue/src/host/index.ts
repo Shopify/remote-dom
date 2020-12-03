e4880371aa788d6e3a00ae4493def25b89b24da3
Chris Sauve <chrismsauve@gmail.com>
2020-12-03 16:56:47 -0500

    Re-export helper types

--- packages/vue/src/host/index.ts
+++ packages/vue/src/host/index.ts
@@ -3,3 +3,7 @@ export {RemoteReceiver} from '@remote-ui/core';
 export {RemoteRenderer} from './RemoteRenderer';
 export {createController} from './controller';
 export type {Controller, ComponentMapping} from './controller';
+export type {
+  VuePropsFromRemoteComponentType,
+  VueComponentTypeFromRemoteComponentType,
+} from '../types';
