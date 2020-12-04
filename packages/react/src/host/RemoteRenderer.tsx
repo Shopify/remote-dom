05ae69aaee43f3285e1fbc7468777c3286085ec6
Chris Sauve <chrismsauve@gmail.com>
2020-12-04 12:22:52 -0500

    Use child kind

--- packages/react/src/host/RemoteRenderer.tsx
+++ packages/react/src/host/RemoteRenderer.tsx
@@ -1,5 +1,5 @@
 import React, {memo, Fragment, createElement} from 'react';
-import type {RemoteReceiver} from '@remote-ui/core';
+import {KIND_COMPONENT, KIND_TEXT, RemoteReceiver} from '@remote-ui/core';
 
 import type {Controller} from './controller';
 import {useAttached} from './hooks';
@@ -17,18 +17,23 @@ export const RemoteRenderer = memo(({controller, receiver}: Props) => {
   return createElement(
     Fragment,
     {},
-    ...children.map((child) =>
-      'type' in child ? (
-        <RemoteComponent
-          key={child.id}
-          component={child}
-          receiver={receiver}
-          controller={controller}
-          __type__={(controller.get(child.type) as any)?.__type__}
-        />
-      ) : (
-        <RemoteText key={child.id} text={child} receiver={receiver} />
-      ),
-    ),
+    ...children.map((child) => {
+      switch (child.kind) {
+        case KIND_COMPONENT:
+          return (
+            <RemoteComponent
+              key={child.id}
+              component={child}
+              receiver={receiver}
+              controller={controller}
+              __type__={(controller.get(child.type) as any)?.__type__}
+            />
+          );
+        case KIND_TEXT:
+          return <RemoteText key={child.id} text={child} receiver={receiver} />;
+        default:
+          return null;
+      }
+    }),
   );
 });
