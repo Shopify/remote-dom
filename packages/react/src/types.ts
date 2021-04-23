ddb9751fbe4a74210b6eee2947025ab770494397
Henry Tao <henry.tao@shopify.com>
2021-04-23 15:05:32 -0400

    Fix ReactPropsFromRemoteComponentType type

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -1,12 +1,23 @@
-import type {ReactNode, ComponentType} from 'react';
-import type {
-  RemoteComponentType,
-  PropsForRemoteComponent,
-} from '@remote-ui/core';
+import type {ReactNode, ComponentType, ReactElement} from 'react';
+import type {RemoteComponentType, RemoteFragment} from '@remote-ui/core';
+
+type PropsForRemoteComponent<T> = T extends RemoteComponentType<
+  string,
+  infer Props,
+  any
+>
+  ? {[K in keyof Props]: RemoteFragmentToReactElement<Props[K]>}
+  : never;
+
+type RemoteFragmentToReactElement<T> = T extends RemoteFragment
+  ? ReactElement
+  : T;
 
 export type ReactPropsFromRemoteComponentType<
   Type extends RemoteComponentType<string, any, any>
-> = PropsForRemoteComponent<Type> & {children?: ReactNode};
+> = PropsForRemoteComponent<Type> & {
+  children?: ReactNode;
+};
 
 export type ReactComponentTypeFromRemoteComponentType<
   Type extends RemoteComponentType<string, any, any>
