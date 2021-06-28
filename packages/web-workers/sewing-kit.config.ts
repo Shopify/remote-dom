e95558eae832ff39885857f0e9c15f374fdd32c5
Chris Sauve <chrismsauve@gmail.com>
2021-06-28 10:46:49 -0400

    Copy wrappers to build directory

--- packages/web-workers/sewing-kit.config.ts
+++ packages/web-workers/sewing-kit.config.ts
@@ -1,5 +1,8 @@
 import {createPackage, Runtime} from '@sewing-kit/config';
+import {createProjectBuildPlugin} from '@sewing-kit/plugins';
 import {defaultProjectPlugin} from '../../config/sewing-kit';
+
+import type {} from '@sewing-kit/plugin-rollup';
 
 export default createPackage((pkg) => {
   pkg.runtimes(Runtime.Node, Runtime.Browser);
@@ -17,4 +20,39 @@ export default createPackage((pkg) => {
     runtime: Runtime.Node,
   });
   pkg.use(defaultProjectPlugin());
+
+  // The library references the worker "wrapper" files as relative paths from
+  // the source code. Because they aren’t in the dependency graph, Rollup
+  // does not bundle them. This sewing-kit plugin adds a copy rollup plugin
+  // that manually moves them into the right build directory.
+  pkg.use(
+    createProjectBuildPlugin('RemoteUI.CopyWrappers', ({hooks, project}) => {
+      hooks.target.hook(({hooks, target}) => {
+        hooks.configure.hook(({rollupPlugins}) => {
+          rollupPlugins.hook(async (plugins) => {
+            // Only add this step for the default variant
+            if (Object.keys(target.options).length > 0) return plugins;
+
+            const {default: copy} = await import('rollup-plugin-copy');
+
+            return [
+              ...plugins,
+              copy({
+                targets: [
+                  {
+                    src: project.fs.resolvePath('src/wrappers/*'),
+                    dest: [
+                      project.fs.buildPath('cjs/wrappers'),
+                      project.fs.buildPath('esm/wrappers'),
+                      project.fs.buildPath('esnext/wrappers'),
+                    ],
+                  },
+                ],
+              }),
+            ];
+          });
+        });
+      });
+    }),
+  );
 });
