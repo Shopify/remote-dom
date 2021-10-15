b9ab0c93b274595dce46061ee74c34bc27f63715
Chris Sauve <chrismsauve@gmail.com>
2021-10-15 15:52:57 -0400

    Use a shared loom plugin for type-check/test

--- loom.config.ts
+++ loom.config.ts
@@ -1,13 +1,11 @@
 import {createWorkspace} from '@shopify/loom';
 import {eslint} from '@shopify/loom-plugin-eslint';
 import {prettier} from '@shopify/loom-plugin-prettier';
-import {workspaceTypeScript} from '@shopify/loom-plugin-typescript';
-import {jest} from '@shopify/loom-plugin-jest';
+import {buildLibraryWorkspace} from '@shopify/loom-plugin-build-library';
 
 export default createWorkspace((workspace) => {
   workspace.use(
-    workspaceTypeScript(),
-    jest(),
+    buildLibraryWorkspace({graphql: false}),
     eslint(),
     prettier({files: '**/*.{md,json,yaml,yml}'}),
   );
