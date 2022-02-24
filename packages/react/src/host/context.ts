6979ddb597a3fec8781c543ef086d6676c4dea1b
Chris Sauve <chrismsauve@gmail.com>
2022-02-24 09:36:38 -0500

    Remove unnecessary useReceiver() and useController() hooks from the React library (#145)

--- packages/react/src/host/context.ts
+++
@@ -1,9 +0,0 @@
-import {createContext} from 'react';
-
-export const ControllerContext = createContext<
-  import('./types').Controller | null
->(null);
-
-export const RemoteReceiverContext = createContext<
-  import('@remote-ui/core').RemoteReceiver | null
->(null);
