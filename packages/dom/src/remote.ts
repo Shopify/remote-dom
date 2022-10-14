54df0345f48b61099c0fd1428e5369ce3b1502d1
Chris Sauve <chrismsauve@gmail.com>
2022-10-13 20:43:36 -0400

    more remote dom

--- packages/dom/src/remote.ts
+++ packages/dom/src/remote.ts
@@ -3,6 +3,7 @@ import type {
   RemoteComponent,
   RemoteText,
   RemoteChild,
+  RemoteComponentType,
 } from '@remote-ui/core';
 
 const NODE_TYPE_ELEMENT = 1;
@@ -10,9 +11,10 @@ const NODE_TYPE_TEXT = 3;
 
 export const INTERNAL_ROOTS = Symbol.for('RemoteUi.Dom.Roots');
 export const INTERNAL_PROPS = Symbol.for('RemoteUi.Dom.Props');
+export const INTERNAL_REMOTE = Symbol.for('RemoteUi.Dom.Remote');
 
 export interface NodeRemoteInternals<RemoteNode extends RemoteChild<any>> {
-  readonly [INTERNAL_ROOTS]: Map<RemoteRoot<any, any>, RemoteNode>;
+  readonly [INTERNAL_ROOTS]: Map<RootElementForRemote, RemoteNode>;
 }
 
 export interface TextRemoteInternals
@@ -37,9 +39,19 @@ interface ElementForRemote
   nodeType: typeof NODE_TYPE_ELEMENT;
 }
 
+export interface RootElementForRemote extends ElementForRemote {
+  readonly [INTERNAL_REMOTE]: {
+    readonly root: RemoteRoot<any, any>;
+    readonly components: Record<string, RemoteDomComponent<any, any, any>>;
+  };
+}
+
 interface ComponentElementForRemote
   extends ElementForRemote,
-    Pick<HTMLElement, 'setAttribute'>,
+    Pick<
+      HTMLElement,
+      'setAttribute' | 'removeAttribute' | 'addEventListener' | 'textContent'
+    >,
     ComponentElementRemoteInternals {}
 
 interface TextForRemote
@@ -54,50 +66,60 @@ export type {
   ComponentElementForRemote as ComponentElement,
 };
 
-export function createElementFromRoot(
+export interface RemoteDomComponent<
+  Type extends string,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Props = {},
+  AllowedChildren extends RemoteComponentType<string, any> | boolean = true,
+> {
+  readonly component: RemoteComponentType<Type, Props, AllowedChildren>;
+}
+
+export function createRemoteDomComponent<
+  Type extends string,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Props = {},
+  AllowedChildren extends RemoteComponentType<string, any> | boolean = true,
+>(
+  component: Type | RemoteComponentType<Type, Props, AllowedChildren>,
+  {}: {} = {},
+): RemoteDomComponent<Type, Props, AllowedChildren> {
+  return {
+    component,
+  };
+}
+
+export function createElementFromRemoteRoot(
   root: RemoteRoot<any, any>,
   {
-    elementToComponent = (name) => name,
-  }: {elementToComponent?(element: string): string} = {},
-) {
-  return createBaseElement('#root', {
+    components = {},
+  }: {components?: Record<string, RemoteDomComponent<any, any, any>>} = {},
+): RootElementForRemote {
+  const baseElement: RootElementForRemote = createBaseElement('#root', {
     appendChild(child) {
-      root.appendChild(nodeToRemote(child as any));
+      root.appendChild(nodeToRemote(child as any, baseElement));
     },
     insertBefore(child, beforeChild) {
+      if (!beforeChild) {
+        root.appendChild(nodeToRemote(child as any, baseElement));
+        return;
+      }
+
       root.insertChildBefore(
-        nodeToRemote(child as any),
-        nodeToRemote(beforeChild as any),
+        nodeToRemote(child as any, baseElement),
+        beforeChild && nodeToRemote(beforeChild as any, baseElement),
       );
     },
     removeChild(child) {
-      root.removeChild(nodeToRemote(child as any));
+      root.removeChild(nodeToRemote(child as any, baseElement));
     },
+  }) as any;
+
+  Object.defineProperty(baseElement, INTERNAL_REMOTE, {
+    value: {root, components},
   });
 
-  function nodeToRemote(
-    node: ComponentElementForRemote | TextForRemote,
-  ): RemoteChild<any> {
-    assertIsRemote(node);
-
-    let remoteChild = node[INTERNAL_ROOTS].get(root);
-
-    if (remoteChild == null) {
-      if (node.nodeType === NODE_TYPE_ELEMENT) {
-        remoteChild = root.createComponent(
-          elementToComponent(node.nodeName),
-          node[INTERNAL_PROPS],
-          Array.from(node.children).map((child) => nodeToRemote(child as any)),
-        );
-      } else {
-        remoteChild = root.createText(node.textContent ?? '');
-      }
-
-      node[INTERNAL_ROOTS].set(root, remoteChild as any);
-    }
-
-    return remoteChild;
-  }
+  return baseElement;
 }
 
 export const document = {
@@ -108,23 +130,25 @@ export const document = {
     const baseElement = createBaseElement(type, {
       appendChild(child) {
         for (const [root, remoteComponent] of roots.entries()) {
-          const remoteChild = child[INTERNAL_ROOTS].get(root);
-          if (remoteChild) remoteComponent.appendChild(remoteChild);
+          remoteComponent.appendChild(nodeToRemote(child as any, root));
         }
       },
       insertBefore(child, beforeChild) {
         for (const [root, remoteComponent] of roots.entries()) {
-          const remoteChild = child[INTERNAL_ROOTS].get(root);
-          const remoteBeforeChild = beforeChild[INTERNAL_ROOTS].get(root);
-          if (remoteChild && remoteBeforeChild) {
+          const remoteChild = nodeToRemote(child as any, root);
+          const remoteBeforeChild =
+            beforeChild && nodeToRemote(beforeChild as any, root);
+
+          if (remoteBeforeChild) {
             remoteComponent.insertChildBefore(remoteChild, remoteBeforeChild);
+          } else {
+            remoteComponent.appendChild(remoteChild);
           }
         }
       },
       removeChild(child) {
         for (const [root, remoteComponent] of roots.entries()) {
-          const remoteChild = child[INTERNAL_ROOTS].get(root);
-          if (remoteChild) remoteComponent.removeChild(remoteChild);
+          remoteComponent.removeChild(nodeToRemote(child as any, root));
         }
       },
     });
@@ -139,8 +163,43 @@ export const document = {
       get [INTERNAL_PROPS]() {
         return props;
       },
+      set textContent(content: string) {
+        for (const child of baseElement.children) {
+          baseElement.removeChild(child);
+        }
+
+        console.log('textContent', content);
+
+        baseElement.appendChild(document.createTextNode(content));
+      },
       setAttribute(name, value) {
+        console.log('setAttribute', name, value);
+
         const newProps = {[name]: value};
+        Object.assign(props, newProps);
+
+        for (const remoteElement of roots.values()) {
+          remoteElement.updateProps(newProps);
+        }
+      },
+      removeAttribute(name) {
+        console.log('removeAttribute', name);
+
+        const newProps = {[name]: undefined};
+        delete (props as any)[name];
+
+        for (const remoteElement of roots.values()) {
+          remoteElement.updateProps(newProps);
+        }
+      },
+      addEventListener(event: string, func: any, options?: any) {
+        console.log('addEventlistener', event, func, options);
+
+        const newProps = {
+          [`on${event[0]!.toUpperCase()}${event.slice(1)}`]: () =>
+            func.call(baseElement, {type: event}),
+        };
+
         Object.assign(props, newProps);
 
         for (const remoteElement of roots.values()) {
@@ -186,9 +245,17 @@ export const document = {
 
     return textNode;
   },
+  createEvent() {
+    return {
+      timeStamp: Date.now(),
+    };
+  },
 };
 
 Reflect.defineProperty(globalThis, 'document', {value: document});
+Reflect.defineProperty(globalThis, 'window', {value: {document}});
+Reflect.defineProperty(globalThis, 'Element', {value: class Element {}});
+Reflect.defineProperty(globalThis, 'SVGElement', {value: class SVGElement {}});
 
 function createBaseElement(
   type: string,
@@ -200,7 +267,7 @@ function createBaseElement(
     appendChild(node: NodeRemoteInternals<RemoteChild<any>>): void;
     insertBefore(
       node: NodeRemoteInternals<RemoteChild<any>>,
-      beforeNode: NodeRemoteInternals<RemoteChild<any>>,
+      beforeNode?: NodeRemoteInternals<RemoteChild<any>> | null,
     ): void;
     removeChild(node: NodeRemoteInternals<RemoteChild<any>>): void;
   },
@@ -222,6 +289,8 @@ function createBaseElement(
       return parentNode.children[children.indexOf(elementNode) + 1] ?? null;
     },
     appendChild(child) {
+      console.log(`appendChild to ${type}`, child);
+
       assertIsRemote(child);
 
       children.push(child);
@@ -232,8 +301,12 @@ function createBaseElement(
       return child;
     },
     insertBefore(childOne, childTwo) {
+      console.log(`insertBefore to ${type}`, childOne, childTwo);
+
+      if (childTwo == null) return elementNode.appendChild(childOne);
+
       assertIsRemote(childOne);
-      assertIsRemote(childTwo);
+      if (childTwo) assertIsRemote(childTwo);
 
       children.splice(children.indexOf(childTwo) - 1, 0, childOne);
       (childOne as any).parentNode = elementNode;
@@ -243,6 +316,8 @@ function createBaseElement(
       return childOne;
     },
     removeChild(child) {
+      console.log(`removeChild to ${type}`, child);
+
       assertIsRemote(child);
 
       if (child.parentNode === (elementNode as any as Node)) {
@@ -258,6 +333,40 @@ function createBaseElement(
   };
 
   return elementNode;
+}
+
+function nodeToRemote(
+  node: ComponentElementForRemote | TextForRemote,
+  rootElement: RootElementForRemote,
+): RemoteChild<any> {
+  assertIsRemote(node);
+
+  let remoteChild = node[INTERNAL_ROOTS].get(rootElement);
+
+  if (remoteChild == null) {
+    const {root, components} = rootElement[INTERNAL_REMOTE];
+
+    if (node.nodeType === NODE_TYPE_ELEMENT) {
+      // TODO: handle missing elements
+      const component = components
+        ? components[node.nodeName]?.component
+        : node.nodeName;
+
+      remoteChild = root.createComponent(
+        component,
+        {...node[INTERNAL_PROPS]},
+        Array.from(node.children).map((child) =>
+          nodeToRemote(child as any, rootElement),
+        ),
+      );
+    } else {
+      remoteChild = root.createText(node.textContent ?? '');
+    }
+
+    node[INTERNAL_ROOTS].set(rootElement, remoteChild as any);
+  }
+
+  return remoteChild;
 }
 
 function assertIsRemote(node: any): asserts node is NodeRemoteInternals<any> {
