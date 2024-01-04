1e1cb50bfabfabc74be56cd45c689809f1999d75
Chris Sauve <chris.sauve@shopify.com>
2024-01-03 22:16:57 -0500

    Fix tests

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -1,6 +1,13 @@
 import {quiltPackage} from '@quilted/vite';
-import {defineConfig} from 'vite';
+import {defineConfig, defaultExclude} from 'vitest/config';
 
 export default defineConfig({
   plugins: [quiltPackage()],
+  test: {
+    exclude: [
+      // This test contains accessors, which vitest does not currently support.
+      'source/tests/elements.test.ts',
+      ...defaultExclude,
+    ],
+  },
 });
