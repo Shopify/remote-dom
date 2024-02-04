695c1bbb4aa3956768745353096671a985c157b6
Chris Sauve <chrismsauve@gmail.com>
2024-02-04 18:32:42 -0500

    More repo cleanup

--- packages/preact/quilt.project.ts
+++
@@ -1,9 +0,0 @@
-import {createProject, quiltPackage} from '@quilted/craft';
-
-export default createProject((project) => {
-  project.use(
-    quiltPackage({
-      react: {importSource: 'preact'},
-    }),
-  );
-});
