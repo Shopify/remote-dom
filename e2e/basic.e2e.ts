51990d83de2eff8669c794962d2bdd23c442fa7d
Robin Drexler <robin.drexler@shopify.com>
2024-11-12 17:45:44 -0500

    first set of e2e tests

---
+++ e2e/basic.e2e.ts
@@ -0,0 +1,36 @@
+import {test, expect} from '@playwright/test';
+
+[
+  ['iframe', 'vanilla'],
+  ['iframe', 'preact'],
+  ['iframe', 'svelte'],
+  ['iframe', 'vue'],
+  ['iframe', 'htm'],
+  // ['iframe', 'react'],
+  // ['worker', 'vanilla'],
+  // ['worker', 'preact'],
+  // ['worker', 'svelte'],
+  // ['worker', 'vue'],
+  // ['worker', 'htm'],
+  // ['worker', 'react'],
+].forEach(([sandbox, example]) => {
+  test(`basic modal interaction with ${sandbox} sandbox and ${example} example`, async ({
+    page,
+  }) => {
+    await page.goto(`/?sandbox=${sandbox}&example=${example}`);
+
+    await page.getByRole('button', {name: 'Open modal'}).click();
+    await page.getByRole('button', {name: 'Click me!'}).click();
+    await page.getByRole('button', {name: 'Click me!'}).click();
+
+    await expect(page.getByText('Click Count: 2')).toBeVisible();
+
+    page.once('dialog', (dialog) => {
+      expect(dialog.message()).toBe('You clicked 2 times!');
+      dialog.dismiss().catch(() => {});
+    });
+
+    await page.getByRole('button', {name: 'Close'}).click();
+    await page.waitForEvent('dialog');
+  });
+});
